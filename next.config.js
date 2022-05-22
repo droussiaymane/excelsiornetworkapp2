dc31dbd73c9800279dcf87a1389df8bba4a9c43e
Aymane Droussi <60798100+droussiaymane@users.noreply.github.com>
2022-05-22 15:13:59 +0100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -7,6 +7,11 @@ module.exports = {
     
     
   },
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
   
  
 }
